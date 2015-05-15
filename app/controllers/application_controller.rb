3f84bf21aec6e8a54b34fc42e290ea9d70879bfa
Brian Armstrong <barmstrong@gmail.com>
2015-05-14 21:57:43 -0700

    more api stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_cookie
+
+  def set_cookie
+    if cookies[:user_id].blank?
+      u = User.create
+      cookies[:user_id] = { value: u.id, expires: 99.years.from_now }
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find(cookies[:user_id]) rescue nil
+  end
 end
