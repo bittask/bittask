82253815c5b2ca29782236fe5b4b35e210ee2953
Charlie Lee <coblee@litecoin.org>
2015-05-15 09:20:00 -0700

    Hook up Coinbase API

---
+++ app/controllers/api/v1/callbacks_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::CallbacksController < ApplicationController
+
+  def index
+    address = params[:address]
+    amount = params[:amount]
+    transaction = params[:transaction]
+
+    puts address
+    puts amount
+    puts transaction
+
+    render json: { success: true }, status: 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       end
       resources :answers
       resources :users
+      resources :callbacks
     end
   end
 
