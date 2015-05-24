874abd8055be3f5b25faf747a7572984348073ce
Brian Armstrong <barmstrong@gmail.com>
2015-05-24 16:24:06 -0700

    merged

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,6 @@
 class StaticController < ApplicationController
 
   def index
-<<<<<<< HEAD
-    @show_header = true
     @task = Task.get_latest(current_user)
   end
 
@@ -15,8 +13,4 @@ class StaticController < ApplicationController
       left join tasks t on t.id = a.task_id
     ")[0]['sum'].to_i / 100
   end
-=======
-    @task = Task.get_latest(current_user)
-  end
->>>>>>> dd6c71f64ccc56f72e64a893f85771a5c678df07
 end
